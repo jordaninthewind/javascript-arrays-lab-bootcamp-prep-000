c3e802b59d8987640e85093d5d4dd52fdbff5298
jordaninthewind <jordan.kline@gmail.com>
2017-09-13 15:08:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1);
+  kittens.slice(1, kittens.length - 1);
   return kittens;
 }
 
 function removeFirstKitten() {
-  kittens.slice(0);
+  kittens.slice(1);
   return kittens;
 }
